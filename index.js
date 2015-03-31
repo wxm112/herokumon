521afc01f887c32ea4cc52cb8241305c45f309d0
王雪梅 <wxm112@me.com>
2015-03-31 13:18:36 +1100

    deleted the comments

--- index.js
+++ index.js
@@ -21,8 +21,6 @@ app.use(function(req, res, next) {
 app.post('/logs', function (req, res) {
   res.send('Got a POST request');
 
-//245 <158>1 2015-03-31T01:36:12.860323+00:00 host heroku router - at=info method=GET path="/login" host=www.data-snippet.io request_id=3ebc158c-4bdd-4325-a279-1c85ef6d7459 fwd="140.168.78.24" dyno=web.1 connect=0ms service=16ms status=200 bytes=2508
-
   lines = req.rawBody.match(/[^\r\n]+/g);
   lines.forEach(function(logLine) {
     if(logLine.match(/host heroku router/)) {
